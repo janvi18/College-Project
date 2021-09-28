479670b33f68d703c144a8346010e663de9c318e
janvi18 <sjanvi9333@gmail.com>
2021-09-28 12:11:18 +0530

    final

--- src/app/currency/currency.component.ts
+++ src/app/currency/currency.component.ts
@@ -1,36 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { RSConverter } from './convertRS'; 
+import { RSConverter } from './convertRS';
 @Component({
   selector: 'app-currency',
   templateUrl: './currency.component.html',
   styleUrls: ['./currency.component.css']
 })
 export class CurrencyComponent implements OnInit {
-  inputNumber =0;
-  objConverter: RSConverter = new RSConverter(0,0,0,0,0);
+  inputNumber = 0;
+  objConverter: RSConverter = new RSConverter(0, 0, 0, 0, 0);
   constructor() { }
-  
+
   ngOnInit(): void {
   }
 
-  currencyConverter()
-  {
-     
-   
+  currencyConverter() {
 
-      if(this.inputNumber <= 0)
-          alert("Enter Value Greaterthan 0");
-      else
-      {
-        var dollar =Number((this.inputNumber/74).toFixed(2));
-        var euro = Number((this.inputNumber/87).toFixed(2));
-        var cdollar = Number((this.inputNumber/59).toFixed(2));
-        var hkdollar = Number((this.inputNumber/9.5).toFixed(2));
-        var mpeso = Number((this.inputNumber/3.6).toFixed(2));
+    if (this.inputNumber <= 0)
+      alert("Enter Value Greaterthan 0");
+    else {
+      var dollar = Number((this.inputNumber / 74).toFixed(2));
+      var euro = Number((this.inputNumber / 87).toFixed(2));
+      var cdollar = Number((this.inputNumber / 59).toFixed(2));
+      var hkdollar = Number((this.inputNumber / 9.5).toFixed(2));
+      var mpeso = Number((this.inputNumber / 3.6).toFixed(2));
 
-        alert(dollar);
-        this.objConverter= new RSConverter(dollar,euro,cdollar,hkdollar,mpeso);
-     
-      }
+      alert(dollar);
+      this.objConverter = new RSConverter(dollar, euro, cdollar, hkdollar, mpeso);
+
+    }
   }
 }
